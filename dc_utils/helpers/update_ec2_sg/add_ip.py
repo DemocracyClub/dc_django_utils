a7461ae51d260592cd9b1a7d6db3ef0a5eae01fb
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-06-10 17:00:31 +0100

    Split the revoke action to its own function
    - Adds ability to remove an IP address from a security group
    individually

--- dc_utils/helpers/update_ec2_sg/add_ip.py
+++ dc_utils/helpers/update_ec2_sg/add_ip.py
@@ -5,49 +5,105 @@ import os
 import requests
 
 
-def get_config():
+def _get_config():
+    """
+    Attempts to read a config file
+    """
     config_dir = os.environ.get("XDG_CONFIG_HOME", f"{os.environ['HOME']}/.config")
     config = configparser.ConfigParser()
     config.read(f"{config_dir}/update_ec2_sg/config.ini")
     return config
 
+
+def _get_client():
+    """
+    Get a boto3 ec2 client instance
+    """
+    return boto3.client("ec2")
+
+
+def get_security_group_id():
+    config = _get_config()
+    security_group_desc = config.get(
+        "SETTINGS", "SECURITY_GROUP_DESC", fallback="ssh_from_dc_admins_ips"
+    )
+    return _get_client().describe_security_groups(
+        Filters=[
+            {
+                "Name": "tag:description",
+                "Values": [security_group_desc],
+            }
+        ]
+    )["SecurityGroups"][0]["GroupId"]
+
+
+def _format_ip_address(ip_address):
+    """
+    Ensure IP address is formatted correctly.
+    TODO allow range other formats?
+    """
+    if not ip_address.endswith("/32"):
+        ip_address += "/32"
+    return ip_address
+
+
+def remove_ip_from_security_group(ip_address=None):
+    """
+    Removes an IP address from the security group ingress rules.
+    """
+    if not ip_address:
+        config = _get_config()
+        ip_address = config.get(
+            "SETTINGS", "IP_ADDRESS", fallback=requests.get("https://ifconfig.me").text
+        )
+
+    return _get_client().revoke_security_group_ingress(
+        GroupId=get_security_group_id(),
+        IpProtocol="tcp",
+        FromPort=22,
+        ToPort=22,
+        CidrIp=_format_ip_address(ip_address),
+    )
+
+
 def add_ip_to_security_group():
     """
     Adds an IP address to the projects SSH admin security groups ingress rules.
     You must have valid AWS credentials configured to run the command. For help
-    logging in using SSO see 
+    logging in using SSO see
     https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-sso.html.
     TODO add documentation for using config file
     """
-    config = get_config()    
-    ip_to_add = config.get("SETTINGS", "IP_ADDRESS", fallback=requests.get("https://ifconfig.me").text)
+    config = _get_config()
+    ip_to_add = config.get(
+        "SETTINGS", "IP_ADDRESS", fallback=requests.get("https://ifconfig.me").text
+    )
     description = config.get("SETTINGS", "DESCRIPTION", fallback=getpass.getuser())
-    security_group_desc = config.get("SETTINGS", "SECURITY_GROUP_DESC", fallback="ssh_from_dc_admins_ips")
+    security_group_desc = config.get(
+        "SETTINGS", "SECURITY_GROUP_DESC", fallback="ssh_from_dc_admins_ips"
+    )
 
-    client = boto3.client("ec2")
+    client = _get_client()
     security_group = client.describe_security_groups(
-        Filters=[{
-            "Name": "tag:description",
-            "Values": [security_group_desc],
-        }]
+        Filters=[
+            {
+                "Name": "tag:description",
+                "Values": [security_group_desc],
+            }
+        ]
     )["SecurityGroups"][0]
 
-    ssh_ips = list(filter(
-        lambda obj: obj['FromPort'] == 22, 
-        security_group["IpPermissions"]
-    ))[0]['IpRanges']
-    ip_to_remove = list(filter(
-        lambda obj: obj["Description"] == description, 
-        ssh_ips,
-    ))
-    if ip_to_remove:
-        client.revoke_security_group_ingress(
-            GroupId=security_group['GroupId'],
-            IpProtocol='tcp',
-            FromPort=22,
-            ToPort=22,
-            CidrIp=ip_to_remove[0]["CidrIp"],
+    ssh_ips = list(
+        filter(lambda obj: obj["FromPort"] == 22, security_group["IpPermissions"])
+    )[0]["IpRanges"]
+    ip_to_remove = list(
+        filter(
+            lambda obj: obj["Description"] == description,
+            ssh_ips,
         )
+    )
+    if ip_to_remove:
+        remove_ip_from_security_group(ip_address=ip_to_remove[0]["CidrIp"])
 
     return client.authorize_security_group_ingress(
         GroupId=security_group["GroupId"],
@@ -56,9 +112,12 @@ def add_ip_to_security_group():
                 "FromPort": 22,
                 "ToPort": 22,
                 "IpProtocol": "tcp",
-                'IpRanges': [
-                    {"CidrIp": f"{ip_to_add}/32", "Description": description},
-                ]
+                "IpRanges": [
+                    {
+                        "CidrIp": _format_ip_address(ip_to_add),
+                        "Description": description,
+                    },
+                ],
             }
-        ]
+        ],
     )
