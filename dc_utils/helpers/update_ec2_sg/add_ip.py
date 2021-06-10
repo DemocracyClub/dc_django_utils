59d364510dfec21ae887e76b74949fbb868e2029
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-06-10 16:29:27 +0100

    Add helpers module, read from config file

--- dc_utils/helpers/update_ec2_sg/add_ip.py
+++ dc_utils/helpers/update_ec2_sg/add_ip.py
@@ -1,33 +1,34 @@
 import boto3
+import configparser
 import getpass
+import os
 import requests
 
 
-def add_ip_to_security_group(ip=None, description=None):
+def get_config():
+    config_dir = os.environ.get("XDG_CONFIG_HOME", f"{os.environ['HOME']}/.config")
+    config = configparser.ConfigParser()
+    config.read(f"{config_dir}/update_ec2_sg/config.ini")
+    return config
+
+def add_ip_to_security_group():
     """
     Adds an IP address to the projects SSH admin security groups ingress rules.
     You must have valid AWS credentials configured to run the command. For help
     logging in using SSO see 
     https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-sso.html.
-    When specifying an IP address to be added, a description must be specified.
-    When IP is not specified, defaults to the current public IP address of the
-    user running the command. 
-    The description defaults to the username of the user running the command.
+    TODO add documentation for using config file
     """
-
-    if ip and not description:
-        raise ValueError(
-            "When specifiying an IP to add you must also specify a description"
-        )
-    
-    ip_to_add = ip or requests.get("https://ifconfig.me").text
-    description = description or getpass.getuser()
+    config = get_config()    
+    ip_to_add = config.get("SETTINGS", "IP_ADDRESS", fallback=requests.get("https://ifconfig.me").text)
+    description = config.get("SETTINGS", "DESCRIPTION", fallback=getpass.getuser())
+    security_group_desc = config.get("SETTINGS", "SECURITY_GROUP_DESC", fallback="ssh_from_dc_admins_ips")
 
     client = boto3.client("ec2")
     security_group = client.describe_security_groups(
         Filters=[{
             "Name": "tag:description",
-            "Values": ["ssh_from_dc_admins_ips"],
+            "Values": [security_group_desc],
         }]
     )["SecurityGroups"][0]
 
