fff346e7ae87140c53f0ae557b340547364f5c57
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-06-11 12:02:19 +0100

    Add entry_points to setup to add ip

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+from importlib.metadata import entry_points
 import json
 import os
 
 from setuptools import setup
 
-setup()
+setup(
+    entry_points = {
+        "console_scripts": ["add_ip_to_sg=helpers.update_ec2_sg.add_ip.add_ip_to_security_group"]
+    }
+)
