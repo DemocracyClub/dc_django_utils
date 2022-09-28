da03460f741a5815647e6f7e6400099c29e7e890
Sym Roe <sym.roe@talusdesign.co.uk>
2022-09-28 16:29:32 +0100

    Add template smoke tests

---
+++ tests/test_templates.py
@@ -0,0 +1,27 @@
+import pytest
+from django.template.loader import select_template
+from djhtml.modes import DjHTML
+
+
+@pytest.mark.parametrize(
+    "template_name",
+    [
+        "dc_base.html",
+        "dc_base_naked.html",
+    ],
+)
+def test_djhtml(template_name):
+    template = select_template([template_name]).template.source
+    mode = DjHTML
+    updated = mode(template).indent(4)
+    assert template == updated
+
+
+def test_dc_base_template(client):
+    req = client.get("/test_dc_base.html")
+    assert req.status_code == 200
+
+
+def test_dc_base_naked_template(client):
+    req = client.get("/test_dc_base_naked.html")
+    assert req.status_code == 200
