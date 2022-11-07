40d208f02af07c00b0ef6821328416313f31d34a
Virginia Dooley <vcdooley@gmail.com>
2022-11-07 15:36:09 +0000

    Change site logo width

--- dc_utils/context_processors.py
+++ dc_utils/context_processors.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 def dc_django_utils(request):
     return {
-        "SITE_LOGO_WIDTH": "300",
+        "SITE_LOGO_WIDTH": "100",
         "SITE_TITLE": getattr(settings, "SITE_TITLE", ""),
         "CANONICAL_URL": f"{request.scheme}://{request.get_host()}",
     }
