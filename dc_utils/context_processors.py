2f3fab4128a0a94ceaa6b95d0365797fb19b0fd6
Virginia Dooley <vcdooley@gmail.com>
2024-01-29 12:19:37 +0000

    Reduce default logo size

--- dc_utils/context_processors.py
+++ dc_utils/context_processors.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 def dc_django_utils(request):
     return {
-        "SITE_LOGO_WIDTH": "100",
+        "SITE_LOGO_WIDTH": "80",
         "SITE_TITLE": getattr(settings, "SITE_TITLE", ""),
         "CANONICAL_URL": f"{request.scheme}://{request.get_host()}",
     }
